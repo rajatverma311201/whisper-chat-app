ceee39435f18a49abd0a4cd8d3ff1b751044d2fa
Rajat Verma <rajat311201@gmail.com>
2025-02-23 02:23:44 +0530

    photo field fix in personal chat controller

--- server/src/controllers/personal-chat-controller.ts
+++ server/src/controllers/personal-chat-controller.ts
@@ -11,11 +11,11 @@ export const getAllMyPersonalChats = catchAsync(async (req, res) => {
     })
         .populate({
             path: "user1",
-            select: "name email about",
+            select: "name email about photo",
         })
         .populate({
             path: "user2",
-            select: "name email about",
+            select: "name email about photo",
         });
 
     res.status(200).json({
